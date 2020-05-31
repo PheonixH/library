79340c0570324a13e6f25e7cd7162b55786e75bb
HuangFeng <19802021118@139.com>
2020-05-31 18:18:08 +0800

    controller: 增加注册

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.pojo.BaseResponse;
+import com.example.demo.pojo.StatusCodeDesc;
 import com.example.demo.pojo.entity.Admin;
 import com.example.demo.service.AdminService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,20 +33,25 @@ public class LoginController {
     AdminService adminService;
 
     @RequestMapping("/index")
-    public String index(){
+    public String index() {
         return "loginPage";
     }
 
     @PostMapping(value = "/registered")
-    public BaseResponse registered(@RequestBody Admin admin) {
-        return adminService.registered(admin);
+    public String registered(@RequestBody Admin admin) {
+        BaseResponse baseResponse = adminService.registered(admin);
+        if (baseResponse.getCode() != StatusCodeDesc.SUCCESS.getCode()) {
+            return "admin";
+        } else {
+            return "loginPage";
+        }
     }
 
 
     @RequestMapping(value = "/adminLogin")
-    public String login(@RequestBody Admin admin, RedirectAttributes model, HttpServletResponse response) {
-        BaseResponse baseResponse =  adminService.login(admin);
-        if (baseResponse.getCode() == 200){
+    public String login(@RequestBody Admin admin, HttpServletResponse response) {
+        BaseResponse baseResponse = adminService.login(admin);
+        if (baseResponse.getCode() == StatusCodeDesc.SUCCESS.getCode()) {
             Cookie cookie = new Cookie("isLogin", "yes");
             // 设置为30min
             cookie.setMaxAge(30 * 60);
@@ -52,7 +59,6 @@ public class LoginController {
             response.addCookie(cookie);
             return "admin";
         } else {
-            model.addFlashAttribute("error", "密码错误");
             return "loginPage";
         }
     }
